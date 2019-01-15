c8bb702318b7997caf5c7d026c909cb2a97ab16f
Joshua Richey <j@joshuar.com>
2019-01-14 18:23:44 -0800

    home styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import { Img } from '../components/image-loader'
+
+// styles
+import '../styles/projects.css'
 //
 
 export default () =>
-  <div>
-    <h1>/* Hilights */</h1>
-    <p>React Native music collaboration app</p>
+  <div className="projects">
+    <section className="summary">
+      <h1>/* Hilights */</h1>
+      <p>A React Native music collaboration app that facilitates conversations about specific sections of recordings.</p>
+      <Img name="jr-xix/hilights/cover.png" />
+    </section>
+
+    <section className="summary">
+      <h1>/* Lumen Learning */</h1>
+      <p>User research, usability testing, and UI wireframes for a LMS (Learning Management System).</p>
+      <Img name="jr-xix/lumen/cover.png" />
+    </section>
+
+  <section className="summary">
+      <h1>/* Idealist */</h1>
+      <p>Idealist.org is an international non-profit job board with a social mission to create more good in the world by helping people turn their intentions into actions. I was their Lead Web Designer for five years.</p>
+      <Img name="jr-xix/idealist/cover.png" />
+    </section>
   </div>
