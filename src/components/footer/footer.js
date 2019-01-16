db5b1299d2d0e4e1e3e31c3f5470cb9fc209d89a
Joshua Richey <j@joshuar.com>
2019-01-16 15:05:04 -0800

    scroll to top arrow

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 // components
 import SocialLinks from '../socialLinks/'
+import ScrollButton from '../scrollToTop/'
 
 // styles
 import './footer.css'
@@ -10,6 +11,10 @@ import './footer.css'
 export default () => {
   return (
     <footer className="footer">
+      <ScrollButton
+        scrollStepInPx="50"
+        delayInMs="16.66"
+      />
       <SocialLinks />
     </footer>
   )
