04c93aa85a745da6e105a8e52d3dc4c1189a5607
Joshua Richey <j@joshuar.com>
2019-01-20 13:53:08 -0800

    little tweaks

--- src/containers/about.js
+++ src/containers/about.js
@@ -21,21 +21,21 @@ export default () => [
       images={['jr-xix/joshua-richey']}
       ratio='1x1'
       duration='1s'
-      className="profile-pic rounded"
+      className="profile-pic rounded bordered"
       indicator
       cloudinary
     />
     </div>
     <section className="bio">
       <p>Joshua Richey is a user experience designer and front-end developer with > 12 years of experience researching, designing, and building websites & applications for humans.</p>
-      <p>He has designed products for Pink Floyd, Skydance Entertainment, and the Grand Canyon Association. He was the lead web designer at Idealist.org for ~5 years, where he spearheaded human centerd design efforts, built and maintained a scalable design system, and rewrote the front end using htlm5, postCSS, and react.</p>
-      <p>When not in front of his screen, he’s probably out walking his retired racing greyhound. When in front of the screen, he probably looks like this:</p>
+      <p>He has designed products for Pink Floyd, Skydance Entertainment, and the Grand Canyon Association, which he thinks is neat. He was the lead web designer at Idealist.org (an international jobs board for nonprofits) for ~5 years, where he spearheaded human centerd design efforts, built and maintained a scalable design system, and rewrote the front end using htlm5, postCSS, and react.</p>
+      <p>When not in front of his screen, he’s probably out walking around with his dog. When in front of the screen, he probably looks like this:</p>
       <div className="working-pic-wrapper">
         <ImgLoad
           images={['jr-xix/working']}
           ratio='1x1'
           duration='1s'
-          className="rounded"
+          className="rounded bordered"
           indicator
           cloudinary
         />
