579a636a5dce6357b2231def020e4242bcea2c59
Joshua Richey <j@joshuar.com>
2019-02-05 14:35:19 -0800

    code demo

--- src/components/codeProject/codeProject.js
+++ src/components/codeProject/codeProject.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import SyntaxHighlighter from 'react-syntax-highlighter';
-import reactElementToJSXString from 'react-element-to-jsx-string';
 
 // components
 import ImgGrid from '../projects/code/imgGrid/'
@@ -11,31 +10,53 @@ import './code-project.css'
 //
 
 
+
+
 class CodeProject extends React.Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      showCode: false
+      showCode: false,
+      error: null,
+      isLoaded: false,
+      text: ''
     }
   }
 
-  getAspectRatio = () => {
-    const computeRatio = (ratio) => {
-      const w = parseInt(ratio.toString().split("x")[0]) // before x
-      const h = parseInt(ratio.toString().split("x")[1]) // after x
-      const aspectRatio = w && h
-        ? `${((h / w) * 100).toFixed(2)}%`
-        : console.log("Incorrect ratio prop")
-      return aspectRatio
-    }
-    const ratio = this.props.ratio && this.props.ratio.length
-      ? computeRatio(this.props.ratio)
-      : null
-    return ratio
+  componentDidMount() {
+    fetch("https://raw.githubusercontent.com/joshuarrr/jr-xix/master/src/components/projects/code/imgGrid/imgGrid.js")
+      .then(res => res.text())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            text: result
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
   }
+
 
   render = () => {
+    const { error, isLoaded, text } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      const codeText = 'Loading...'
+    } else {
+      const codeText = text
+    }
+
     return (
       <section
         className="project-summary"
@@ -56,25 +77,33 @@ class CodeProject extends React.Component {
           { !this.state.showCode
             ? this.props.children
             : <div className="code-pane">
-                <SyntaxHighlighter
-                  language='javascript'
-                  showLineNumbers
-                >
-                  {reactElementToJSXString(
-                    <ImgGrid
-                      url='jr-xix/projects/santa-monica'
-                      ratio='16x9'
-                      duration='.5s'
-                      className="rounded bordered"
-                      cloudinary
-                    />
-                  )}
-                </SyntaxHighlighter>
+                  <SyntaxHighlighter
+                    language='javascript'
+                    showLineNumbers
+                  >
+                    {this.state.text}
+                  </SyntaxHighlighter>
               </div>
           }
         </div>
       </section>
     )
+  }
+
+
+  getAspectRatio = () => {
+    const computeRatio = (ratio) => {
+      const w = parseInt(ratio.toString().split("x")[0]) // before x
+      const h = parseInt(ratio.toString().split("x")[1]) // after x
+      const aspectRatio = w && h
+        ? `${((h / w) * 100).toFixed(2)}%`
+        : console.log("Incorrect ratio prop")
+      return aspectRatio
+    }
+    const ratio = this.props.ratio && this.props.ratio.length
+      ? computeRatio(this.props.ratio)
+      : null
+    return ratio
   }
 }
 
